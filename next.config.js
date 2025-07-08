e5a7f2b86279337db92b91491425bac09ead5a05
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-07-07 17:44:40 -0700

    next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
+// /** @type {import('next').NextConfig} */
+
+// const isProd = process.env.NODE_ENV === 'production'
+
+// const nextConfig = {
+//   // reactStrictMode: true,
+//   swcMinify: true,
+//   assetPrefix: isProd ? '/love-island-visualizations/' : ''
+
+// }
+
+// export default nextConfig
+
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  // reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/love-island-visualizations/' : ''
+  reactStrictMode: true,
+  assetPrefix: '', // remove or leave as empty string
+};
 
-}
-
-export default nextConfig
+export default nextConfig;
